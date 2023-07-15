332ef270874c2fe8ae04b5168f9bd07ef10efdfa
Sajad Adil <73715889+Sajad-Adil@users.noreply.github.com>
2023-07-15 10:29:15 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        new
-        <h1 v-for="item in x" :key="item">{{ item }}</h1>
+    <div  >
+        <form class="flex justify-center items-center u-screen m-6"  @submit.prevent="addName">
+            <label for="name">Name:</label>
+            <input class="border border-red-300 rounded px-3 py-1" type="text" v-model="name" id="name" />
+            <button class="bg-red-500 hover:bg-red-800 text-white rounded px-4 py-1 ml-2" type="submit">Enter</button>
+        </form>
+
+        <ol type="1">
+            <li v-for="nameItem in names" :key="nameItem"  class="flex justify-center items-center u-screen">
+                {{ nameItem }}
+            </li>
+        </ol>
     </div>
 </template>
 
-<script setup >
-const x = ref([1, 2, 3, 4])
+<script setup>
 
+const names = ref([])
+const name = ref('')
 
-
+const addName = () => {
+    if (name.value) {
+        names.value.push(name.value)
+        name.value = ''
+    }
+};
 </script>
-
